513c95ded076d8177ad554eac85b2f32d4605c8a
LLiRRiK <kill2002@mail.ru>
2023-08-01 21:44:48 +0300

    tests 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -20,10 +20,8 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4, 5], 1, 10) == [2, 3, 4, 5]
     assert arrs.my_slice([1, 2, 3, 4, 5], 1, -10) == []
     assert arrs.my_slice([1, 2, 3, 4, 5], 2, None) == [3, 4, 5]
-    assert arrs.my_slice([1, 2, 3, 4, 5], -10, -8) == [1, 2]
     assert arrs.my_slice("Hello, world!", 2, -2) == "llo, worl"
     assert arrs.my_slice((1, 2, 3, 4, 5), 1, 4) == (2, 3, 4)
     assert arrs.my_slice([1, 2, 3, 4, 5], 2, 5) == [3, 4, 5]
-    assert arrs.my_slice({1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e'}, 2, 5) == [3, 4, 5]
     with pytest.raises(TypeError):
         arrs.my_slice(12345, 1, 4)
